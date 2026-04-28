1b1043f883846a1f2e88bd1b1d19a6b37875c9ae
Erwan Cloux <erwanclou@gmail.com>
2026-04-28 10:56:30 +0200

    Add dynamic cam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -24,9 +25,21 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject GameOverScreen;
 
+    [SerializeField] private Transform CameraTransform;
+    [SerializeField] private float TiltAngle = 10f;
+    [SerializeField] private float TiltSpeed = 5f;
+    private float _currentTilt;
+
     void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
+    }
+
+    private void Update()
+    {
+        float target = -_movement.x * TiltAngle;
+        _currentTilt += (target - _currentTilt) * Time.deltaTime * TiltSpeed;
+        CameraTransform.localEulerAngles = new Vector3(0f, 0f, _currentTilt);
     }
 
     private void FixedUpdate()
