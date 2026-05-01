ae548edb84b6c33ee3dad8c155aeb018231c592b
Enimoop <marine.forcioli@laposte.net>
2026-05-01 13:02:42 +0200

    Tir Scene

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,10 +4,14 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 
 {
+    [SerializeField]
+    private GameObject Spawner;
+
     public void PlayAgain()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Score.score = 0;
+        Spawner.SetActive(true);
     }
 
     public void Quit()
